6f9b1227ad3ccab84e0cc036bd4f68f2273a92b6
honestMan <dcao5105@gmail.com>
2026-01-15 14:35:00 +0800

    update

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -1,30 +1,30 @@
 window.RULES_DIRECT = {
-  "/": {
-    url: "https://blog.dmsoul.com/",
-    expired_at: "",
-  },
-  "/long": {
-    url: "https://iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.in/",
-    expired_at: "",
-  },
-  "/esa": {
-    url: "https://tianchi.aliyun.com/specials/promotion/freetier/esa?taskCode=25254&recordId=c856e61228828a0423417a767828d166",
-    expired_at: "",
-  },
-  "/s": {
-    url: "https://7s.nz/_url",
-    expired_at: "",
-  },
-  "/mail": {
-    url: "https://mail.dmsoul.com",
-    expired_at: "",
-  },
-  "/nav": {
-    url: "https://nav.dmsoul.com",
-    expired_at: "",
-  },
-  "/imgbed": {
-    url: "https://imgbed.dmsoul.com",
-    expired_at: "",
-  },
+    "/": {
+        "url": "https://blog.dmsoul.com/",
+        "expired_at": ""
+    },
+    "/long": {
+        "url": "https://iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.in/",
+        "expired_at": ""
+    },
+    "/esa": {
+        "url": "https://tianchi.aliyun.com/specials/promotion/freetier/esa?taskCode=25254&recordId=c856e61228828a0423417a767828d166",
+        "expired_at": ""
+    },
+    "/s": {
+        "url": "https://7s.nz/_url",
+        "expired_at": ""
+    },
+    "/mail": {
+        "url": "https://mail.dmsoul.com",
+        "expired_at": ""
+    },
+    "/nav": {
+        "url": "https://nav.dmsoul.com",
+        "expired_at": ""
+    },
+    "/imgbed": {
+        "url": "https://imgbed.dmsoul.com",
+        "expired_at": ""
+    }
 };
--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1,34 +1,34 @@
 window.RULES_INTERMEDIATE = {
-  "/githello": {
-    url: "https://github.com",
-    expired_at: "",
-  },
-  "/weibo": {
-    url: "https://weibo.com",
-    expired_at: "",
-  },
-  "/qq": {
-    url: "https://qq.com",
-    expired_at: "",
-  },
-  "/baidu": {
-    url: "https://www.baidu.com/",
-    expired_at: "",
-  },
-  "/google": {
-    url: "https://www.google.com",
-    expired_at: "",
-  },
-  "/telegram": {
-    url: "https://web.telegram.org",
-    expired_at: "",
-  },
-  "/deepseek": {
-    url: "https://chat.deepseek.com",
-    expired_at: "",
-  },
-  "/bilibili": {
-    url: "https://www.bilibili.com",
-    expired_at: "",
-  },
+    "/githello": {
+        "url": "https://github.com",
+        "expired_at": ""
+    },
+    "/weibo": {
+        "url": "https://weibo.com",
+        "expired_at": ""
+    },
+    "/qq": {
+        "url": "https://qq.com",
+        "expired_at": ""
+    },
+    "/baidu": {
+        "url": "https://www.baidu.com/",
+        "expired_at": ""
+    },
+    "/google": {
+        "url": "https://www.google.com",
+        "expired_at": ""
+    },
+    "/telegram": {
+        "url": "https://web.telegram.org",
+        "expired_at": ""
+    },
+    "/deepseek": {
+        "url": "https://chat.deepseek.com",
+        "expired_at": ""
+    },
+    "/bilibili": {
+        "url": "https://www.bilibili.com",
+        "expired_at": ""
+    }
 };
