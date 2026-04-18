3d475778a6500363df141a7876fc94791688dd19
Dreamer <dcao5105@gmail.com>
2026-04-18 11:32:19 +0800

    Update rules_direct.js

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -39,7 +39,7 @@ window.RULES_DIRECT = {
         "url": "https://cpa.7s.nz/management.html",
         "expired_at": ""
     },"/xs": {
-        "url": "https://novel.dmsoul.com",
+        "url": "https://xiaoshuo.7s.nz/",
         "expired_at": ""
     },
 };
